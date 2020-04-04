645af9ae0ab346db5f47dfd7dcbb1b476eb37248
thaithanhdat125 <tichpro2611999@gmail.com>
2020-04-04 22:14:21 +0700

    Initial commit

---
+++ app/src/main/java/vn/edu/ntu/thaithanhdat_59130291_59cntt3/MainActivity.java
@@ -0,0 +1,14 @@
+package vn.edu.ntu.thaithanhdat_59130291_59cntt3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
